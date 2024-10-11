c50a47b5a07ee23ac8afce9b311de3ed4f0492f7
Promeroguaq <paolaromero829@yahoo.com>
2024-10-10 20:52:44 -0500

    Ejericio-huevo-frito-2

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log("Conseguir un huevo");
+console.log("Conseguir una sartén");
+console.log("Conseguir una espátula");
+console.log("Encender la estufa");
+console.log("Colocar en la estufa la sartén para que se vaya calentando");
+console.log("Añadir el huevo");
+console.log("Añadir sal al huevo");
+console.log("Esperar que cocine el huevo");
+console.log("Apagar la estufa");
+
+// Definir la variable panOGalletas con un valor (puede ser "pan", "galletas", u otro valor)
+let panOGalletas = "pan"; // O cambiar por "galletas" para otra condición
+
+if (panOGalletas == "pan") {
+    console.log("Encender la estufa");
+    console.log("Colocar el pan en la estufa");
+    console.log("Esperar a que se dore el pan");
+    console.log("Apagar la estufa");
+} else if (panOGalletas == "galletas") {
+    console.log("Servir las galletas");
+} else {
+    console.log("Buscar pan o galletas");
+}
+
+console.log("Coger un plato");
+console.log("Servir el huevo");
+console.log("Comer");
